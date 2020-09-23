b635271baf9d63516f63d034a393d6f18622908d
Bobae Kang <bkang.dev@gmail.com>
2020-09-23 12:04:53 -0500

    Use RegisterForm inside Modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { useState } from "react";
 import Modal from "./Modal";
+import RegisterForm from "./RegisterForm";
 
 function App() {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <h1>Portal Registration Modal</h1>
-      <button onClick={() => setShowModal(true)}>Open modal</button>
+      <div style={{ textAlign: "center" }}>
+        <h1>Portal Registration Modal</h1>
+        <button onClick={() => setShowModal(true)}>Open modal</button>
+      </div>
       <Modal show={showModal}>
-        <div
-          style={{
-            backgroundColor: "#fff",
-            height: "300px",
-            width: "300px",
-            padding: "1rem",
-          }}
-        >
-          <p>I'm in Modal!</p>
-          <button onClick={() => setShowModal(false)}>Close</button>
-        </div>
+        <RegisterForm onClose={() => setShowModal(false)} />
       </Modal>
     </>
   );
